709aa366d2fe6f228c962e8632775fce33f25bb0
tchandr1 <tchandr1@hawk.iit.edu>
2017-01-14 16:56:01 -0600

    Added a button to add new event

--- calender-controller.js
+++ calender-controller.js
@@ -3,19 +3,20 @@
  */
 (function(){
     'use strict';
-    angular.module('calender-app',['mwl.calendar', 'ui.bootstrap']);
+    angular.module('calender-app',['mwl.calendar', 'ui.bootstrap','ngAnimate', 'colorpicker.module']);
 
     angular.module('calender-app').controller('CalenderController',CalenderControllerFn)
 
-    CalenderControllerFn.$inject = [];
-    function CalenderControllerFn(){
+    CalenderControllerFn.$inject = ['calendarConfig','moment'];
+    function CalenderControllerFn(calendarConfig,moment){
         var ccVm =this;
 
+        //attributes of calender needed to display calender
         ccVm.calendarView = 'month';
         ccVm.currentDate = new Date();
-        ccVm.title = "Current Month View"
         ccVm.cellIsOpen = true;
 
+        //Event added to current date
         ccVm.events = [
             {
                 title: 'My Birthday',
@@ -27,24 +28,16 @@
         ];
 
 
-        ccVm.timespanClicked = function(date, cell) {
-
-            if (ccVm.calendarView === 'month') {
-                if ((ccVm.cellIsOpen && moment(date).startOf('day').isSame(moment(ccVm.viewDate).startOf('day'))) || cell.events.length === 0 || !cell.inMonth) {
-                    ccVm.cellIsOpen = false;
-                } else {
-                    ccVm.cellIsOpen = true;
-                    ccVm.viewDate = date;
-                }
-            } else if (ccVm.calendarView === 'year') {
-                if ((ccVm.cellIsOpen && moment(date).startOf('month').isSame(moment(ccVm.viewDate).startOf('month'))) || cell.events.length === 0) {
-                    ccVm.cellIsOpen = false;
-                } else {
-                    ccVm.cellIsOpen = true;
-                    ccVm.viewDate = date;
-                }
-            }
-        }
+        ccVm.addEvent = function() {
+            ccVm.events.push({
+                title: 'Type new event',
+                startsAt: moment().startOf('day').toDate(),
+                endsAt: moment().endOf('day').toDate(),
+                color: calendarConfig.colorTypes.important,
+                draggable: true,
+                resizable: true
+            });
+        };
     }
 
 
