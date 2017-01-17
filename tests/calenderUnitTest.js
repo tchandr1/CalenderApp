1dfb99a17e81193e9a96956c9b36c1a1734362ef
tchandr1 <tchandr1@hawk.iit.edu>
2017-01-17 01:47:00 -0600

    Added initial unit testcases

--- tests/calenderUnitTest.js
+++ tests/calenderUnitTest.js
@@ -15,19 +15,53 @@ describe('calenderApp', function() {
     beforeEach(inject(function($controller) {
         ccVm = $controller("CalenderController");
    }));
-    ccVm.events = {
-        title: "SprintPlanning",
-        color: {
-            primary: "#ad2121",
-            secondary: "#fae3e3"
-        },
-        startsAt: "Fri Jan 13 2017 08:00:00 GMT-0600",
-        endsAt: "Wed Jan 18 2017 23:59:59 GMT-0600",
-        draggable: true
-    }
-    var x= 10;
 
-    it('should create new event row', function() {
-        expect(x).toBe(10);
+
+
+    describe('create new row', function() {
+        it('should create new event row', function () {
+            var events = {
+                title: "SprintPlanning",
+                color: {
+                    primary: "#ad2121",
+                    secondary: "#fae3e3"
+                },
+                startsAt: "Fri Jan 13 2017 08:00:00 GMT-0600",
+                endsAt: "Wed Jan 18 2017 23:59:59 GMT-0600",
+                draggable: true
+            }
+            expect(events.title).toBe("SprintPlanning");
+        });
+    });
+
+
+
+    describe('Check Clashing events', function() {
+        it('should validate whether event exists', function () {
+            var events = [{
+                title: "SprintPlanning1",
+                color: {
+                    primary: "#ad2121",
+                    secondary: "#fae3e3"
+                },
+                startsAt: "Fri Jan 13 2017 08:00:00 GMT-0600",
+                endsAt: "Wed Jan 18 2017 23:59:59 GMT-0600",
+                draggable: true
+            },
+                {
+                    title: "SprintPlanning2",
+                    color: {
+                        primary: "#ad2121",
+                        secondary: "#fae3e3"
+                    },
+                    startsAt: "Fri Jan 13 2017 08:00:00 GMT-0600",
+                    endsAt: "Wed Jan 18 2017 23:59:59 GMT-0600",
+                    draggable: true
+                }
+            ]
+            ccVm.validate(events)
+            expect(ccVm.hasDuplicates()).toBe(true);
+
+        });
     });
 });
